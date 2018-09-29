0ea4f775947826c84a5ef920067cce9d1a686ca0
skuzbot <steven.k.duncan@gmail.com>
2018-09-28 20:07:11 -0500

    Try to render messages --no-verify

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,12 @@ class App {
     this.friends = {};
   }
 
-  init() {}
+  init() {
+    $(".get-messages").on('click', function(event){
+      this.fetch();
+      console.log('hi')
+    })
+  }
 
   send(data) {
     $.ajax({
@@ -25,23 +30,44 @@ class App {
   }
 
   fetch() {
-    $.ajax({
+    var request = $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
       contentType: 'application/json',
+      success: function (data) {
+        for(var i = 0; i < data.results.length; i++) {
+          this.renderMessage(data.results[i]);
+        }
+        console.log('chatterbox: Message received');
+        console.log(data.results[0]);  
+        // {results:[... 100 arrays]}  
+          // {objectId: "wlYrCX99Vc", username: "dan", text: "first", roomname: "lobby", createdAt: "2017-02-08T21:17:18.455Z", …}
+        //App.renderMessage(data.results[0])
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
     });
+    // console.log(request)
+    // var data = request.responseJSON.results  // array
+    // this.renderMessage(data[0])
 
   }
   
+  getMessages() {
+
+  }
   
   clearMessages() {
     $('#chats').empty();
   }
   
   renderMessage(message) {
-    let str = `<div class='${message.username} username' >${message.username} : ${message.text}</div>`;
+    let str = `<div class='username' >${message.username} : ${message.text}</div>`;
     $('#chats').append(str);
+
   }
   
   renderRoom(roomName) {
@@ -50,8 +76,8 @@ class App {
   }
 
   handleUsernameClick() {
-    $('#chats').on('click', '.username', function(event) {
-      alert('hello');
+    $('#main').on('click', '.username', function(event) {
+      console.log('need to add friend');
     });
   }
 }
